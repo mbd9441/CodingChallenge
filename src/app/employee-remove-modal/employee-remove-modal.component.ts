3a8556de84f2318e4e8499d850bbff2dee2176ad
Matthew Dunn <mbd9441@rit.edu>
2019-11-28 05:56:34 -0500

    added add report row to employees with no reports and fixed delete modal bug

--- src/app/employee-remove-modal/employee-remove-modal.component.ts
+++ src/app/employee-remove-modal/employee-remove-modal.component.ts
@@ -12,17 +12,18 @@ export class EmployeeRemoveModalComponent implements OnInit {
   private fromEmployee: Employee;
   
   ngOnInit(): void {
-    this.employee=this.data.employee;
+    //this.employee=this.data.employee;
   }
 
   constructor(
     public dialogRef: MatDialogRef<EmployeeRemoveModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
-      if (!!data){
-        this.employee=data[0];
+      if (!!data.employee){
+        this.employee=data.employee[0];
         console.log("anus");
-        if (data[1]){
-          this.fromEmployee=data[1];
+        console.log(data);
+        if (data.employee[1]){
+          this.fromEmployee=data.employee[1];
           console.log("Remove gdcgf" + this.employee.id + " from " + this.fromEmployee.id);
         }
       }
